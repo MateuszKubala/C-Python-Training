4e3b269dc34ec42f6230c8f2df1da64099706208
MateuszKubala <101786143+MateuszKubala@users.noreply.github.com>
2023-02-08 19:35:17 +0100

    Update obliczanie liczb 1233 v2.py

--- obliczanie liczb 1233 v2.py
+++ obliczanie liczb 1233 v2.py
@@ -8,7 +8,7 @@ for a in range (10000, 100000, 1):
         f=int(e)
         g=int(c)+int(d)
     if f==g:
-        print("tak kurwa", f,"=",g)
+        print("wynik: ", f,"=",g)
     else:
         x=0
             
