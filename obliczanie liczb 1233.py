694b20293d1154ac91d91fc3f8f0714cf0a143b6
MateuszKubala <101786143+MateuszKubala@users.noreply.github.com>
2024-09-23 17:39:47 +0200

    Obliczanie superliczb.py

--- obliczanie liczb 1233.py
+++ obliczanie liczb 1233.py
@@ -1,18 +1,23 @@
 def main():
+    # Zakres od użytkownika
+    start = int(input("Podaj początkową wartość zakresu (min. 10000): "))
+    end = int(input("Podaj końcową wartość zakresu (np. 999999): "))
 
+    # Minimalna wartość
+    if start < 10000:
+        print("Początkowa wartość została ustawiona na 10000.")
+        start = 10000
 
-    j=0
-    for i in range(10000,1000000,1):
-        a=i//1000
-        b=i%1000
-        c=a*a+b*b
+    j = 0
+    for i in range(start, end + 1):
+        a = i // 1000  # Pierwsze trzy cyfry liczby i, dzielenie całkowite
+        b = i % 1000   # Ostatnie trzy cyfry, reszta z dzielenia
+        c = a * a + b * b
         if c == i:
-            print("super ", c,"=",i)
-            j=j+1
-            print("liczba =", j)
-        
+            print("Super liczba:", i, "=", c)
+            j += 1
+            print("a= ", a, " b= ", b, " c= ",c)
 
-
-
+    print("Liczba super liczb:", j)
 
 main()
